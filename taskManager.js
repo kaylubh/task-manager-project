4058dd55f2fe02f0f1430e9da5bd2807a6eebcb6
Caleb Hemphill <calebhemp@gmail.com>
2026-04-28 19:57:52 -0400

    Add task actions

--- taskManager.js
+++ taskManager.js
@@ -1,6 +1,7 @@
-// import { Task } from './task.js';
-// import { createTaskRow } from './taskElement.js';
+// Task Manager Application
 
+/* ------------------------------------------------------------ */
+// Global Variables
 // Tasks
 const tasks = [];
 // DOM Elements
@@ -39,6 +40,7 @@ class Task {
 
   get taskDetails() {
     return {
+      id: this.#id,
       name: this.#name,
       priority: this.#priority,
       dateCreated: this.#dateCreated,
@@ -77,23 +79,30 @@ class Task {
   }
 }
 
+/* ------------------------------------------------------------ */
 // Create Task Element
-function createTaskRow(taskDetails) {
-  const taskRow = document.createElement('tr');
 
+// Create a table row for a task
+function createTaskRow(taskDetails) {
+  // Create table row and cells for task details (name, priority, date) and action buttons
+  const taskRow = document.createElement('tr');
   const nameCell = createTaskCell(taskDetails.name);
   const priorityCell = createTaskCell(taskDetails.priority);
   const dateCell = createTaskDateCell(taskDetails.dateCreated);
-  const actionButtonsCell = createActionsButtons();
+  const actionButtonsCell = createActionsButtons(taskDetails);
 
+  // Append cells to the task row
   taskRow.appendChild(nameCell);
   taskRow.appendChild(priorityCell);
   taskRow.appendChild(dateCell);
   taskRow.appendChild(actionButtonsCell);
 
+  
+
   return taskRow;
 }
 
+// Helper functions to create table cells for name, priority, and date
 function createTaskCell(content) {
   const cell = document.createElement('td');
   cell.innerHTML = content;
@@ -109,25 +118,42 @@ function createTaskDateCell(date) {
   return cell;
 }
 
-function createActionsButtons() {
+// Helper function to create action buttons (Complete, Edit, Delete)
+function createActionsButtons(taskDetails) {
+  const taskId = taskDetails.id;
+  const taskCompleted = taskDetails.isCompleted;
+  const taskImportant = taskDetails.isImportant;
+
   // Action buttons group
   const cell = document.createElement('td');
   const buttonGroup = document.createElement('div');
-  buttonGroup.classList.add('btn-group');
+  buttonGroup.classList.add('actions-group');
 
-  // Complete Button
-  const completeButton = document.createElement('button');
-  completeButton.innerHTML = 'Complete';
-  buttonGroup.appendChild(completeButton);
+  // Complete Checkbox
+  const completeCheckbox = document.createElement('input');
+  completeCheckbox.type = 'checkbox';
+  completeCheckbox.classList.add('complete-checkbox');
+  completeCheckbox.id = `${taskId}`;
+  if (taskCompleted) {
+    completeCheckbox.checked = true;
+  }
+  buttonGroup.appendChild(completeCheckbox);
 
-  // Edit Button
-  const editButton = document.createElement('button');
-  editButton.innerHTML = 'Edit';
-  buttonGroup.appendChild(editButton);
+  // Important Checkbox
+  const importantCheckbox = document.createElement('input');
+  importantCheckbox.type = 'checkbox';
+  importantCheckbox.classList.add('important-checkbox');
+  importantCheckbox.id = `${taskId}`;
+  if (taskImportant) {
+    importantCheckbox.checked = true;
+  }
+  buttonGroup.appendChild(importantCheckbox);
 
   // Delete Button
   const deleteButton = document.createElement('button');
   deleteButton.innerHTML = 'Delete';
+  deleteButton.classList.add('delete-button');
+  deleteButton.id = `${taskId}`;
   buttonGroup.appendChild(deleteButton);
 
   cell.appendChild(buttonGroup);
@@ -135,27 +161,9 @@ function createActionsButtons() {
   return cell;
 }
 
-// Add Task
-taskAddForm.addEventListener('submit', (event) => {
-  event.preventDefault();
-
-  const nameInput = document.querySelector('#task-name');
-  const priorityInput = document.querySelector('#task-priority');
-  const importantInput = document.querySelector('#task-important');
-
-  const name = nameInput.value.trim();
-  const priority = priorityInput.value;
-  const isImportant = importantInput.checked;
-
-  if (name) {
-    const newTask = new Task(name, priority, isImportant);
-    tasks.push(newTask);
-    displayTaskList(tasks);
-    taskAddForm.reset();
-  }
-});
-
-function displayTaskList(tasks) {
+/* ------------------------------------------------------------ */
+// Display Task List
+function displayTaskList() {
   // reset task list
   taskTableBody.innerHTML = '';
 
@@ -169,4 +177,85 @@ function displayTaskList(tasks) {
   console.log(JSON.stringify(tasks, null, 2));
 }
 
-//
+/* ------------------------------------------------------------ */
+// Add Task
+
+// Event listener for task add form submission
+taskAddForm.addEventListener('submit', (event) => {
+  event.preventDefault();
+
+  // Get form input values
+  const nameInput = document.querySelector('#task-name');
+  const priorityInput = document.querySelector('#task-priority');
+  const importantInput = document.querySelector('#task-important');
+
+  const name = nameInput.value.trim();
+  const priority = priorityInput.value;
+  const isImportant = importantInput.checked;
+
+  // Create a new task and add it to the tasks array if the name is not empty
+  if (name) {
+    const newTask = new Task(name, priority, isImportant);
+    tasks.push(newTask);
+    displayTaskList(); // Update the task list display
+    taskAddForm.reset();
+  }
+});
+
+/* ------------------------------------------------------------ */
+// Task Actions (Complete, Edit, Delete)
+
+// Complete Task
+function toggleTaskCompletion(taskId) {
+  // Find the task in the tasks array using the task ID
+  const taskToComplete = tasks.find((task) => task.id === taskId);
+  // Get the current completion status of the task
+  const isCompleted = taskToComplete.taskDetails.isCompleted;
+
+  // Toggle the completion status of the task
+  taskToComplete.isCompleted = !isCompleted;
+}
+
+// Toggle Important Status
+function toggleTaskImportant(taskId) {
+  // Find the task in the tasks array using the task ID
+  const taskToToggle = tasks.find((task) => task.id === taskId);
+  // Get the current important status of the task
+  const isImportant = taskToToggle.taskDetails.isImportant;
+
+  // Toggle the important status of the task
+  taskToToggle.isImportant = !isImportant;
+}
+
+// Delete Task
+function deleteTask(taskId) {
+  // Find the index of the task in the tasks array using the task ID
+  const taskIndex = tasks.findIndex((task) => task.id === taskId);
+  // Remove the task from the tasks array
+  if (taskIndex !== -1) {
+    tasks.splice(taskIndex, 1);
+  }
+}
+
+// Event Listener for Task Actions
+taskTableBody.addEventListener('click', (event) => {
+  const taskId = event.target.id; // get the task ID
+
+  // Check if and which action was clicked using event delegation
+  if (event.target.tagName === 'BUTTON') {
+    // Delete Button
+    if (event.target.classList.contains('delete-button')) {
+      deleteTask(taskId);
+    }
+  } else if (event.target.tagName === 'INPUT') {
+    // Complete Checkbox
+    if (event.target.classList.contains('complete-checkbox')) {
+      toggleTaskCompletion(taskId);
+    } else if (event.target.classList.contains('important-checkbox')) {
+      // Toggle Important Status
+      toggleTaskImportant(taskId);
+    }
+  }
+
+  displayTaskList(); // Update the task list display after any action
+});
